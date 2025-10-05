39a876dece6d03ee67a5e72842be0ef3d6c4ddd5
mohamed khounti <larbikhounti@gmail.com>
2025-10-05 14:37:25 +0100

    feat(cdn): add public access decorator to dome client-side endpoint

--- backend/src/cdn/cdn.controller.ts
+++ backend/src/cdn/cdn.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { CdnService } from './cdn.service';
+import { Public } from '@app/decorator/public.decorator';
 
 @Controller('cdn')
 export class CdnController {
   constructor(private readonly cdnService: CdnService) {}
 
+  @Public()
   @Get('dome-client-side')
   async getDomeClientSide(@Res() res: Response): Promise<void> {
     return this.cdnService.serveDomeClientSide(res);
